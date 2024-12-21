da4bf29ff105c718cfce4ad0c5f558ccd1a4c234
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-12-21 17:43:20 +0500

    basic of node js

---
+++ basicsNodejs/script.js
@@ -0,0 +1,61 @@
+ const { log } = require('console');
+ 
+const fs = require('fs');
+
+// Write file
+// fs.writeFile("hey.txt","hello kaise ho",function(err){
+//  if(err) console.log(err);
+//  else console.log("done");
+
+// })
+
+// Append File mai kuch bhi nahi badalta
+// likay huway main ahay add karna
+
+// fs.appendFile("hey.txt"," main tu ho",function(err){
+//  if(err) console.log(err);
+//  else console.log("done");
+
+// })
+
+// Rename
+
+
+// fs.rename("hello txt","hello.txt",function(err){
+//     if(err) console.log(err);
+//     else console.log("done");
+   
+//    })
+
+
+// copy file
+
+
+// fs.copyFile("hello.txt","./Copy/chacha.txt",function(err){
+//     if(err) console.log(err);
+//     else console.log("done");
+   
+//    })
+
+
+// unlink file ko remove karta h
+
+// fs.unlink("hello.txt",function(err){
+//     if(err) console.error(err)
+//         else console.log("remove")
+// })
+
+// rm folder remove 
+
+// fs.rm("./Copy",{recursive:true},function(err){
+//     if(err) console.error(err)
+//      else console.log("remove")
+    
+// })
+
+
+// http or https protocol hai internet kay 
+// yahi ek protocol hai ya rules jismain agar issay use nahi karogay tu 
+// nahi app kuch bech sakhte ho or nahi mangwa sakte ho 
+// yeh windaows main hote hain
+
