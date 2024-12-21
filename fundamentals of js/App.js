1c168aa90cd5473f514354dc6b76135162a4a3c4
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-12-21 17:44:11 +0500

    fundamentals of JS

---
+++ fundamentals of js/App.js
@@ -0,0 +1,68 @@
+// Fundamentals Of Javascript:
+// Array And Object:
+// forEach map and filter find indexOf
+// function return
+// async js coding
+
+// var arr=[1,2,3,4]
+// arr.forEach(function(val){
+//     console.log(val+ " hello zain welcom back in js");
+// })
+
+//map tab use hota hao jab app apny ek array se naya arrya banaty hain
+
+//  var arr=[1,2,3,4]
+
+// var newarr=arr.map(function(val){
+// return 69
+
+// })
+// console.log(newarr);
+
+//filter
+
+
+//  var arr=[1,2,3,4]
+
+// var newarr=arr.filter(function(val){
+// if(val >= 3){return true};
+
+// })
+// console.log(newarr);
+
+
+//find array main kuch dhondnay ka tariqa
+
+
+//  var arr=[1,2,3,4]
+
+// var newarr=arr.find(function(val){
+// if(val==2)
+//     return val;
+
+// })
+// console.log(newarr);
+
+
+
+// OBJECTS
+
+// var obj={
+//     name:"zain",
+//     age:20,
+// }
+// obj.name
+// obj.age
+
+//FUNCTION
+// function addNumbers(a, b) {
+//     return a + b;
+//   }
+  
+//   var result = addNumbers(5, 10);
+//   console.log(result); 
+
+  
+
+
+//Async js coding
