48197613f8fa8be0d02916b6e5e196f23ec24ceb
ZAIN ALI SHAH <155235083+Zain-4321@users.noreply.github.com>
2024-12-23 00:27:32 +0500

    Npm Basics

---
+++ Npm basic Advanced/script.js
@@ -0,0 +1,9 @@
+// NPM BASIC AND ADVANCED
+// npm--node pakage manager 
+// node is related sare pakages milaingay
+// node js core main jo installed ata hai wo hai module
+// npm se jo bhi download karaty hain wo hai package
+// kai bar app ek script chalatay ho or uss script main ap yeh likhytay ho
+// npm start
+// npm rub dev
+// npm concurrent
